9b4cc1c51d235622170eee5ed5adb8cdf72f42a0
PrajnaBh <49976513+PrajnaBh@users.noreply.github.com>
2021-08-07 15:59:10 +0530

    Add files via upload

---
+++ src/stores/store.js
@@ -0,0 +1,23 @@
+import { createStore } from 'vuex';
+
+const store = createStore({
+    state() {
+        return {
+            userId: null,
+        }
+    },
+
+    mutations: {
+
+    },
+
+    actions: {
+
+    },
+
+    getters: {
+
+    }
+})
+
+export default store;
